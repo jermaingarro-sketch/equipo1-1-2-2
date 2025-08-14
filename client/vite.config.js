1827d1edb94d2a6724db791163dfd66cd8831f3a
unknown <jermandrey@gmail.com>
2025-08-14 09:26:26 -0500

    ignore backend3 bn

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Asegúrate de que esté así para Vercel
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets' // Carpeta donde se guardarán los recursos
+}})
